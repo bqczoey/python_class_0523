1b31e568910c48dab01c4f4016a7293d70b7d14a
bqczoey <zhu920302@gmail.com>
2026-06-13 11:55:57 +0800

    0613 upload

--- 0606/lesson6.py
+++ 0606/lesson6.py
@@ -18,7 +18,8 @@ def main():
             # list[dict] -> DataFrame
             df:DataFrame = pd.DataFrame(data=data)
             
-            output_file = Path(__file__).with_name("youbike_report.pdf")
+            output_file = Path(__file__).with_name("youbike_report.pdf"
+)
 
             report.export_to_pdf(df, output_file)
     else:
